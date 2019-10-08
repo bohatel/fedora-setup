c18c6a37ba722ea6ab77866b16575c541963d049
razvan.bohatel@albeado.com <razvan.bohatel@albeado.com>
2019-10-08 13:15:05 +0300

    packadge updates

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -2,8 +2,8 @@
 cd ~
 # First we setup the repositories so we can install lots of stuff
 # Set Virtual Box repo
-wget https://raw.githubusercontent.com/ChrisTitusTech/fedora-setup/master/fedora.packages
-wget https://raw.githubusercontent.com/ChrisTitusTech/fedora-setup/master/bashrc.aliases
+wget https://raw.githubusercontent.com/bohatel/fedora-setup/master/fedora.packages
+wget https://raw.githubusercontent.com/bohatel/fedora-setup/master/bashrc.aliases
 # Install Google Chrome repo
 sudo dnf install fedora-workstation-repositories -y
 # Install Rpmfusion repo
@@ -13,10 +13,6 @@ sudo rpm --import https://packagecloud.io/AtomEditor/atom/gpgkey
 sudo sh -c 'echo -e "[Atom]\nname=Atom Editor\nbaseurl=https://packagecloud.io/AtomEditor/atom/el/7/\$basearch\nenabled=1\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://packagecloud.io/AtomEditor/atom/gpgkey" > /etc/yum.repos.d/atom.repo'
 # Update cache for package installs
 sudo dnf makecache
-# Install Dropbox
-sudo dnf install https://www.dropbox.com/download?dl=packages/fedora/nautilus-dropbox-2015.10.28-1.fedora.x86_64.rpm -y
-# Install TeamViewer
-sudo dnf install https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm -y
 # grab all packages to install
 sudo dnf install $(cat ~/fedora.packages) -y
 
@@ -24,4 +20,5 @@ sudo dnf install $(cat ~/fedora.packages) -y
 cat ~/bashrc.aliases >> ~/.bashrc
 
 #Disable Wayland and use Xorg
-sudo sed -i '/WaylandEnable/s/^#//g' /etc/gdm/custom.conf
+#Using KDE so this is not needed 
+#sudo sed -i '/WaylandEnable/s/^#//g' /etc/gdm/custom.conf
